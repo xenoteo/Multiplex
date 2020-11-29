c104f14aec9bc629665efd08ce8aa0a1450146e1
enkelian <aniano13@gmail.com>
2020-11-29 17:25:37 +0100

    added number to hall

--- src/main/java/agh/alleviation/model/user/Customer.java
+++ src/main/java/agh/alleviation/model/user/Customer.java
@@ -1,9 +1,13 @@
 package agh.alleviation.model.user;
 
+import agh.alleviation.model.Order;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 /**
  * @author Anna Nosek
@@ -15,6 +19,13 @@ import javax.persistence.Table;
 public class Customer extends User{
     public static final String TABLE_NAME = "customer";
 
+
+    private final ObjectProperty<List<Order>> ordersProperty = new SimpleObjectProperty<>();
+
+//    @OneToMany
+//    List<Order> getOrders(){
+//        retur
+//    }
 
 
 }
