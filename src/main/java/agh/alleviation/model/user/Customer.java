6c68f385158a54855b15926fab29030cbb48ec71
enkelian <aniano13@gmail.com>
2020-11-29 17:45:02 +0100

    adding orders to user

--- src/main/java/agh/alleviation/model/user/Customer.java
+++ src/main/java/agh/alleviation/model/user/Customer.java
@@ -3,10 +3,15 @@ package agh.alleviation.model.user;
 import agh.alleviation.model.Order;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import org.springframework.data.repository.cdi.Eager;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.List;
 
 /**
@@ -22,10 +27,35 @@ public class Customer extends User{
 
     private final ObjectProperty<List<Order>> ordersProperty = new SimpleObjectProperty<>();
 
-//    @OneToMany
-//    List<Order> getOrders(){
-//        retur
-//    }
+    @OneToMany(fetch = FetchType.EAGER)
+    public List<Order> getOrders(){
+        return ordersProperty.get();
+    }
+
+    public void setOrders(List<Order> orders){
+        ordersProperty.set(orders);
+    }
+
+    public ObjectProperty<List<Order>> ordersProperty(){
+        return ordersProperty;
+    }
+
+    public void addOrder(Order order){
+        getOrders().add(order);
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        out.writeObject(getOrders());
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        setOrders((List<Order>) in.readObject());
+
+    }
 
 
 }
